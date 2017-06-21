8aae4bdb8a70cc88b6e21251091852126b72cf01
Sh1nes00n <sh1nes00n@live.com>
2017-06-21 17:54:48 +0800

    完成1001题

---
+++ 乙级/1001. 害死人不偿命的(3n+1)猜想 (15).cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace PAT
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = Convert.ToInt32(Console.ReadLine());
+            int step = 0;
+            while (num != 1)
+            {
+                if (num % 2 == 0)
+                {
+                    num /= 2;
+                    step++;
+                }
+                else
+                {
+                    num = (num * 3 + 1) / 2;
+                    step++;
+                }
+            }
+            Console.WriteLine(step);
+        }
+    }
+}
